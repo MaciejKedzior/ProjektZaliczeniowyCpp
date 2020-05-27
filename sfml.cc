bcbd1a5a42bcb30f81c056f6aab81ebabebb84da
MaciejKedzior <maciej.kedzior@student.uj.edu.pl>
2020-05-27 02:45:18 +0200

    wstepne autorskie grafiki

--- sfml.cc
+++ sfml.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <algorithm>
 #include <fstream>
 #include <vector>
@@ -13,27 +14,30 @@
 
 
 int main(void){
-    std::vector <sf::Text> values;
+    std::vector <sf::Text> temp_values;
     std::vector <Card*> cards;
+    std::array <sf::Text, 3> to_be_printed;
     sf::Font font;
-    sf::Text nazwa, punkty, opis;
+    int number_of_cards;
     int index = 0;
 
     
     cards.emplace_back(new BliskieStarcie("Geralt z Rivii", "Wiedzmin i gbur", 15, "geralt.jpg"));
     cards.emplace_back(new DalekiZasieg("Keira Metz", "Czarodziejka z Temerii", 5, "keiraok.jpg"));
-    cards.emplace_back(new JednostkaObl("Katapulta", "Oh jak ogromna!", 8, "katapulta.jpg"));
+    cards.emplace_back(new JednostkaObl("Balista", "Oh jak ogromna!", 6, "balista.jpg"));
     cards.emplace_back(new BliskieStarcie("Komandos Niebieskich Pasow", "Chlop na schwal", 3, "komandos.jpg"));
+    cards.emplace_back(new DalekiZasieg("Yennefer", "Najwieksza milosc Geralta", 7, "yennefer.jpg"));
+    number_of_cards = cards.size()-1;
 
-    
-    values = cards.at(index)->getValues();
+
+    temp_values = cards.at(index)->getValues();
     sf::Sprite sprite = cards.at(index)->getGraphics();
 
     font = loadFont("arial.ttf");
 
-    nazwa = createTextBox(values.at(0), 25, &font, sf::Vector2f(500.f, 150.f));
-    punkty = createTextBox(values.at(1), 25, &font, sf::Vector2f(500.f, 190.f));
-    opis = createTextBox (values.at(2), 15, &font, sf::Vector2f(530.f, 270.f));
+    to_be_printed[0] = createTextBox(temp_values.at(0), 25, &font, sf::Vector2f(500.f, 150.f));
+    to_be_printed[1] = createTextBox(temp_values.at(1), 25, &font, sf::Vector2f(500.f, 190.f));
+    to_be_printed[2] = createTextBox(temp_values.at(2), 15, &font, sf::Vector2f(530.f, 270.f));
     
 
     sf::RenderWindow window(sf::VideoMode(1024.f, 656.f), "Gwint - talia");
@@ -76,25 +80,25 @@ int main(void){
                             if (index == 0) continue;
                             else {
                                 sprite = cards.at(--index)->getGraphics();
-                                values = cards.at(index)->getValues();
+                                temp_values = cards.at(index)->getValues();
 
                                 cards.at(index)->showInfo();
-
-                                nazwa = createTextBox(values.at(0), 25, &font, sf::Vector2f(500.f, 150.f));
-                                punkty = createTextBox(values.at(1), 25, &font, sf::Vector2f(500.f, 190.f));
-                                opis = createTextBox(values.at(2), 15, &font, sf::Vector2f(530.f, 270.f));
+                                
+                                to_be_printed[0] =createTextBox(temp_values.at(0), 25, &font, sf::Vector2f(500.f, 150.f));
+                                to_be_printed[1] = createTextBox(temp_values.at(1), 25, &font, sf::Vector2f(500.f, 190.f));
+                                to_be_printed[2] = createTextBox(temp_values.at(2), 15, &font, sf::Vector2f(530.f, 270.f));
                             }
                         }
                         if(forward.contains(position)){
-                            if (index == 3) continue;
+                            if (index == number_of_cards) continue;
                             else {
                                 sprite = cards.at(++index)->getGraphics();
-                                values = cards.at(index)->getValues();
+                                temp_values = cards.at(index)->getValues();
                                 cards.at(index)->showInfo();
                                 
-                                nazwa = createTextBox(values.at(0), 25, &font, sf::Vector2f(500.f, 150.f));
-                                punkty = createTextBox(values.at(1), 25, &font, sf::Vector2f(500.f, 190.f));
-                                opis = createTextBox(values.at(2), 15, &font, sf::Vector2f(530.f, 270.f));
+                                to_be_printed[0] = createTextBox(temp_values.at(0), 25, &font, sf::Vector2f(500.f, 150.f));
+                                to_be_printed[1] = createTextBox(temp_values.at(1), 25, &font, sf::Vector2f(500.f, 190.f));
+                                to_be_printed[2] = createTextBox(temp_values.at(2), 15, &font, sf::Vector2f(530.f, 270.f));
                             }
                         }
                         if(menu.contains(position)) std::cout<<"Powrót do menu głównego!\n";
@@ -109,9 +113,9 @@ int main(void){
         window.draw(triangle_back); window.draw(triangle_fwd);
         window.draw(main_menu);
         window.draw(sprite);
-        window.draw(nazwa);
-        window.draw(punkty);
-        window.draw(opis);
+        window.draw(to_be_printed[0]);
+        window.draw(to_be_printed[1]);
+        window.draw(to_be_printed[2]);
         
         window.display();
     }
